383fda5be53c3b9d7e3b81064d852da6b7ea89cf
MostafaAmrHamed <mostafaamro5619@gmail.com>
2020-10-08 19:33:37 +0200

    made first content card

--- wuzzuf-clone-spa/src/components/Job-page/jobPage-container.js
+++ wuzzuf-clone-spa/src/components/Job-page/jobPage-container.js
@@ -6,11 +6,13 @@ class JobPage extends Component {
     state = {
         title: 'Computer Science Researcher - Internship',
         companyName: 'Digital Cloud Platforms',
-        jobDate:'5',
+        jobDate:'5 days ago',
         applicants:'65',
         consideration: '15',
         notSelected: '21',
-        viewed:'35'
+        viewed:'35',
+        openPosition:'1',
+        screenWidth: window.innerWidth,
     }
 
     render() {
--- wuzzuf-clone-spa/src/components/Job-page/jobPage-view.js
+++ wuzzuf-clone-spa/src/components/Job-page/jobPage-view.js
@@ -1,22 +1,67 @@
 import React from 'react';
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import { GoVerified } from 'react-icons/go';
+
 import './jobPage-style.css'
 
 function JobPage(props) {
     const state = props.state
-    return(
+    return (
         <Container className="job-page">
-            <div className="content-card">
-                <Row>
-                    <Col lg={9} md={8}>
-                        <h2>{state.title}</h2>
-                    </Col>
-                    
-                    <Col lg={3} md={4}>
-                        <Image src="https://s3-eu-west-1.amazonaws.com/wuzzuf/files/company_logo/Digital-Cloud-Platforms-Egypt-41008-1552924240-sm.png"/>
-                    </Col>
-                </Row>
-            </div>
+
+            <Row>
+                <Col md={8} xs={12}>
+                    <div className="content-card">
+                        <Row>
+                            <Col lg={9} md={8}>
+                                <h3>{state.title}</h3>
+                            </Col>
+
+                            <Col lg={3} md={4}>
+                                <Image src="https://s3-eu-west-1.amazonaws.com/wuzzuf/files/company_logo/Digital-Cloud-Platforms-Egypt-41008-1552924240-sm.png"  />
+                            </Col>
+                        </Row>
+                        <p className="companyName">{state.companyName} <GoVerified className="verified" /></p>
+                        <p className="text-muted">Posted {state.jobDate}</p>
+                        <div className="applyInfo">
+                            <Row>
+                                <Col lg={4} className="borderR">
+                                    <Button className="btn-Apply"> Apply to Job </Button>
+                                </Col>
+
+                                <Col lg={3} className="applicants borderR">
+                                    <span className="num"> {state.applicants} </span>
+                                    <p>
+                                        Applicants for {state.openPosition} open position
+                                    </p>
+                                </Col>
+
+                                <Col lg={5}>
+                                    <div className="stat">
+                                        <div className="applicants-num"> {state.viewed} </div>
+                                        <div className="applicants-desc"> Viewed </div>
+                                    </div>
+                                    <div className="stat">
+                                        <div className="applicants-num"> {state.consideration} </div>
+                                        <div className="applicants-desc" style={{color:'green'}}> In Consideration </div>
+                                    </div>
+                                    <div className="stat">
+                                        <div className="applicants-num"> {state.notSelected} </div>
+                                        <div className="applicants-desc" style={{color:'red'}}> Not Selected </div>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                </Col>
+
+                <Col md={4} xs={12}>
+                    <h1>Hello, how are you! hehehehehe</h1>
+                </Col>
+                {
+                    console.log(state.screenWidth)
+                }
+            </Row>
         </Container>
     );
 }
