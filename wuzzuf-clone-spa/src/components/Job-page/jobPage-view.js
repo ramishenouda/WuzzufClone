824f958fccb61133508e2518c290f6ff335f4853
MostafaAmrHamed <mostafaamro5619@gmail.com>
2020-10-17 20:34:48 +0200

    made the first card more responsive

--- wuzzuf-clone-spa/src/components/Job-page/jobPage-view.js
+++ wuzzuf-clone-spa/src/components/Job-page/jobPage-view.js
@@ -8,12 +8,11 @@ function JobPage(props) {
     const state = props.state
     return (
         <Container className="job-page">
-
             <Row>
                 <Col md={8} xs={12}>
                     <div className="content-card">
                         <Row>
-                            <Col lg={9} md={8}>
+                            <Col lg={9} md={8}  className="title">
                                 <h3>{state.title}</h3>
                             </Col>
 
@@ -21,22 +20,24 @@ function JobPage(props) {
                                 <Image src="https://s3-eu-west-1.amazonaws.com/wuzzuf/files/company_logo/Digital-Cloud-Platforms-Egypt-41008-1552924240-sm.png"  />
                             </Col>
                         </Row>
+
                         <p className="companyName">{state.companyName} <GoVerified className="verified" /></p>
                         <p className="text-muted">Posted {state.jobDate}</p>
+
                         <div className="applyInfo">
                             <Row>
-                                <Col lg={4} className="borderR">
+                                <Col lg={4}  xs={12} className="borderR buttonApply">
                                     <Button className="btn-Apply"> Apply to Job </Button>
                                 </Col>
 
-                                <Col lg={3} className="applicants borderR">
+                                <Col lg={3}  xs={12} className="applicants borderR">
                                     <span className="num"> {state.applicants} </span>
                                     <p>
                                         Applicants for {state.openPosition} open position
                                     </p>
                                 </Col>
 
-                                <Col lg={5}>
+                                <Col lg={5} xs={12} className="stats">
                                     <div className="stat">
                                         <div className="applicants-num"> {state.viewed} </div>
                                         <div className="applicants-desc"> Viewed </div>
@@ -52,6 +53,7 @@ function JobPage(props) {
                                 </Col>
                             </Row>
                         </div>
+                        
                     </div>
                 </Col>
 
